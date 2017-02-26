f2366e5971bf5acbdf794613dcbad9010cd64842
Ronald Coe <ron@rc3ipad.coegroupllc.com>
2017-02-25 20:14:45 -0500

    Initial Commit

---
+++ sampleRCJTutorial/sampleRCJTutorial_webhook.py
@@ -0,0 +1,6 @@
+{
+	"name" : "Sample RCJ Tutorial Webhook",
+	"targetUrl" : "https://script.google.com/macros/s/AKfycbyNrHolaoje5rurIuNS7d-fKAYRM5oqwsV5xg113JpB6_eldow/exec",
+	"resource" : "messages",
+	"event" : "created"
+}
