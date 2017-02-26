9c79a7d57d0866be87709e29d955ed6bf0e411f3
Ronald Coe <ron@rc3ipad.coegroupllc.com>
2017-02-25 20:16:41 -0500

    Corrected Initial Commit

---
+++ sampleRCJTutorial/sampleRCJTutorial.py
@@ -0,0 +1,14 @@
+'''
+    By: Ronald Coe, Jr.
+    Date: February 25, 2017
+    Description:
+    '''
+import sys
+
+
+def simple_http_server(my_arguments):
+    print "A server definition would start here."
+
+
+if __name__ == '__main__':
+    simple_http_server()
