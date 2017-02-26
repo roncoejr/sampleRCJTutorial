8fd9997d04cc5fd348a581dba36769a2065b5ffb
Ronald Coe <ron@rc3ipad.coegroupllc.com>
2017-02-25 20:56:01 -0500

    changes

--- sampleRCJTutorial/sampleRCJTutorial.py
+++ sampleRCJTutorial/sampleRCJTutorial.py
@@ -2,27 +2,29 @@
     By: Ronald Coe, Jr.
     Date: February 25, 2017
     Description:
-    '''
+'''
 import sys
 
 
 def simple_http_server():
     '''
         The servers GET/POST logic will go here
-        '''
+    '''
     print "A server definition would start here."
 
 def server_hello_response():
     '''
         The server's response to the initial greeting will be processed here
-        '''
+    '''
     print "I do beleive that I've been spoken to!"
 
 def server_room_invite_response():
     '''
         When the BOT is added to a room this suite will handle responding to that event
-        '''
+    '''
     print "I have been added to a room"
 
 if __name__ == '__main__':
+    '''
+    '''
     simple_http_server()
---
+++ sampleRCJTutorial/starter.py
@@ -0,0 +1,17 @@
+import sampleRCJTutorial
+
+'''
+'''
+
+def main():
+    '''
+    '''
+    sampleRCJTutorial.simple_http_server()
+    sampleRCJTutorial.server_hello_response()
+    sampleRCJTutorial.server_room_invite_response()
+
+
+if __name__ == '__main__':
+    '''
+    '''
+    main()
